7e922fa22b4614e641b8f628f6159aa771bf024e
Byeclouds <chinesedema@qq.com>
2023-03-26 15:49:48 +0800

    Get password

---
+++ prac_02/password_stars.py
@@ -0,0 +1,34 @@
+"""
+MIN_LENRTH = 10
+user_password = str(input("Enter password:"))
+while len(user_password) < MIN_LENRTH:
+    print("Invalid password")
+    user_password = str(input("Enter password:"))
+else:
+    print("*" * len(user_password))
+"""
+
+MIN_LENGTH = 10
+
+
+def get_password():
+    """Get user's password print stars."""
+    user_password = input("Enter password: ")
+    while len(user_password) < MIN_LENGTH:
+        print(f"Invalid password, at least {MIN_LENGTH} words.")
+        user_password = input("Enter password: ")
+    return user_password
+
+
+def display_stars(user_password):
+    """Print stars."""
+    print('*' * len(user_password))
+
+
+def main():
+    """Display password or stars."""
+    password = get_password()
+    display_stars(password)
+
+
+main()
