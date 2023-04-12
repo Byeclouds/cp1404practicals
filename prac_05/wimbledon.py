a4f072ae561e7e20dd6c624c3c1a8deb22065f0a
Byeclouds <chinesedema@qq.com>
2023-04-12 11:11:23 +0800

    Complete prac 5 wimbledon exercise

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,50 @@
+FILENAME = "wimbledon.csv"
+
+
+def main():
+    # Main function that runs the program.
+    data = get_data(FILENAME)
+    champions = get_champions(data)
+    countries = list(get_countries(data))
+    countries.sort()
+
+    print("Wimbledon Champions: ")
+    for champions, count in champions.items():  # for each champion and count in the dictionary
+        print(f"{champions} {count}")
+    print()
+    print(f"These are {len(countries)} countries have won Wimbledon: ")
+    print(",".join(countries))
+
+
+def get_data(filename):
+    # Reads a CSV file and returns its data as a list of lists.
+    data = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            row = line.strip().split(',')
+            data.append(row)
+    return data
+
+
+def get_champions(data):
+    # Counts the number of championships won by each player.
+    champions = {}
+    for row in data:
+        champion = row[2]
+        if champion in champions:
+            champions[champion] += 1
+        else:
+            champions[champion] = 1
+    return champions
+
+
+def get_countries(data):
+    # Extracts the unique set of countries from the CSV data.
+    countries = set()
+    for row in data:
+        countries.add(row[1])
+    return countries
+
+
+main()
