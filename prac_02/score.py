7d1e0ca6886dc10c123f67d030ac9a7624e24089
Byeclouds <chinesedema@qq.com>
2023-11-18 16:18:51 +0800

    score

---
+++ prac_02/score.py
@@ -0,0 +1,32 @@
+"""
+score = float(input("Enter score: "))
+if score < 0 or score > 100:
+    print("Invalid score")
+elif score >= 90:
+    print("Excellent")
+elif score >= 50:
+    print("Passable")
+else:
+    print("Bad")
+"""
+
+
+def main():
+    """Get a score with its status."""
+    score = float(input("Enter score: "))
+    print(get_status(score))
+
+
+def get_status(score):
+    """Get the score‘s status. """
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
